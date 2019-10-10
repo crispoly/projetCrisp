7b6070c3ebeb6aa92f9960e840ead594c07bae04
crispoly <38506751+crispoly@users.noreply.github.com>
2019-10-10 14:21:11 -0400

    Update send_other.m

--- MainScript/Communication/communication_private/send_other.m
+++ MainScript/Communication/communication_private/send_other.m
@@ -1,12 +1,12 @@
 function [ reply_dg ] = send_other( command_dg )
 %send_other Envoi une commande inhabituel sur le bus.
-%   La fonction écrit le datagram de commande dans le field «free» de la
-%   memory map et attend la réponse. La commande est ensuite effacée.
+%   La fonction Ã©crit le datagram de commande dans le field Â«freeÂ» de la
+%   memory map et attend la rÃ©ponse. La commande est ensuite effacÃ©e.
 
 global map;
 empty = uint8(zeros(1,9));
 
-write_dg( map, 'free', command_dg, 1 );
+write_dg( map, "free", command_dg, 1 );
 
 finish = false;
 while ~finish
@@ -14,7 +14,7 @@ while ~finish
     reply_dg = read_dg(map, 'free', 2);
     finish = ~isequal(reply_dg, empty);
 end
-write_dg( map, 'free', empty, 2 );
+write_dg( map, "free", empty, 2 );
 
 
 end
